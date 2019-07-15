4839c8cce61deef102de7a591713913f82d9d295
John Dunn <jdunn33@cnm.edu>
2019-07-15 17:37:49 -0600

    added initial information, added initial class information

--- sql/php/Classes/foo.php
+++ sql/php/Classes/foo.php
@@ -1,3 +1,10 @@
 <?php
 namespace jdunn33;
-//Trait to
+//Adding Class//
+class author implements /JsonSerializable {
+	use ValidateAuthor;
+	use ValidateAuthorId;
+	//id for the author; this is the primary key//
+	private $authorId;
+
+}
