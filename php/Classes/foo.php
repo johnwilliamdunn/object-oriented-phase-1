681173c39b691b86a73d5a23457356bd9fe16527
John Dunn <jdunn33@cnm.edu>
2019-07-16 16:03:36 -0600

    added block notation, adding accessor methods, added public function, added autoload script, adding script for mutator

--- php/Classes/foo.php
+++ php/Classes/foo.php
@@ -71,5 +71,59 @@ class author implements \JsonSerializable {
 	public function getauthorUserName
 	    return ($this->authorUserName);
 
-
+public function __set($authorId, $value)
+{
+	$mutator = 'set' . ucfirst($authorId);
+	if (method_exists($authorId, $mutator) && is_callable(array($authorId, $mutator)))
+	{
+		$authorId->$mutator($value);
+	}
+	throw new Exception("{$authorId} is an invalid property.");
+}
+public function __set($authorAvatarUrl, $value)
+{
+	$mutator = 'set' . ucfirst($AuthorAvatarUrl);
+	if (method_exists($authorAvatarUrl, $mutator) && is_callable(array($authorAvatarUrl, $mutator)))
+	{
+		$authorAvatarUrl->$mutator($value);
+	}
+	throw new Exception("{$authorAvatarUrl} is an invalid property.");
+}
+public function __set($name, $value)
+{
+	$mutator = 'set' . ucfirst($authorActivationToken);
+	if (method_exists($authorActivationToken, $mutator) && is_callable(array($authorActivationToken, $mutator)))
+	{
+		$authorActivationToken->$mutator($value);
+	}
+	throw new Exception("{$authorActivationToken} is an invalid property.");
+}
+public function __set($authorEmail, $value)
+{
+	$mutator = 'set' . ucfirst($authorEmail);
+	if (method_exists($authorEmail, $mutator) && is_callable(array($authorEmail, $mutator)))
+	{
+		$this->$mutator($value);
+	}
+	throw new Exception("{$authorEmail} is an invalid property.");
+}
+public function __set($authorHash, $value)
+{
+	$mutator = 'set' . ucfirst($authorHash);
+	if (method_exists($authorHash, $mutator) && is_callable(array($authorHash, $mutator)))
+	{
+		$authorHash->$mutator($value);
+	}
+	throw new Exception("{$authorHash} is an invalid property.");
+}
+public function __set($authorUsername, $value)
+{
+	$mutator = 'set' . ucfirst($authorUsername);
+	if (method_exists($authorUsername, $mutator) && is_callable(array($authorUsername, $mutator)))
+	{
+		$authorUsername->$mutator($value);
+	}
+	throw new Exception("{$authorUsername} is an invalid property.");
+}
+}
 }
