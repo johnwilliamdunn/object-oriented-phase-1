6a29e39d6f033977792547ae31c4529a9a0e7b75
John Dunn <jdunn33@cnm.edu>
2019-07-15 20:12:01 -0600

    added initial information, added initial class information, added private authorid

--- sql/php/Classes/foo.php
+++ sql/php/Classes/foo.php
@@ -4,7 +4,12 @@ namespace jdunn33;
 class author implements /JsonSerializable {
 	use ValidateAuthor;
 	use ValidateAuthorId;
-	//id for the author; this is the primary key//
+	/**id for the author; this is the primary key
+	 * @var Uuid $authorId
+	 **/
 	private $authorId;
+/** id of the Author that is sent, this is the Foriegn Key
+ *
+ **/
 
 }
