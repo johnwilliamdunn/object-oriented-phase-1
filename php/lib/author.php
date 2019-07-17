3cf3d3b0a14776a82f1431407cf360735901d9e8
John Dunn <jdunn33@cnm.edu>
2019-07-17 15:22:29 -0600

    added block notation, adding accessor methods, added public function, added autoload script, adding script for mutator, added autoload scripts, added brackets, removed mutator code, changed Uuid to string variable, values changed for foriegn key, starting constructor, adding constructor parameters, adding param for variables, added throw values, adding values for setting parameters, input of argument exception for constructor, adding mutators, added invalid argument exception, adding activation token string, changed filename syntax, writing author hash/passwrd, wrote author hash/passwrd, wrote author user name, writing json array, wrote json array, writing instantiation, writing instantiation, writing instantiation

--- php/lib/author.php
+++ php/lib/author.php
@@ -2,4 +2,7 @@
 
 require_once("../Classes/Author.php");
 
-$johndunn = new Author; ()
+$johndunn = new Author; ("910f2599-125f-441c-ab23-40438b9ff118", "https://cdn1.creativecirclemedia.com/taosnews/original/20161230-184922-9bdbf8abf616d929293aae1a27cc993d.jpg", "activation token", "jdunn33@cnm.ed", "passwrd", "jdunn33");
+
+$johndunn->do_Author();
+?>
