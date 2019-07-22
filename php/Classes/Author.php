eb983a8f5ba062c8f0f57f74c01c515530f50d1a
John Dunn <jdunn33@cnm.edu>
2019-07-22 16:19:00 -0600

    added block notation, adding accessor methods, added public function, added autoload script, adding script for mutator, added autoload scripts, added brackets, removed mutator code, changed Uuid to string variable, values changed for foriegn key, starting constructor, adding constructor parameters, adding param for variables, added throw values, adding values for setting parameters, input of argument exception for constructor, adding mutators, added invalid argument exception, adding activation token string, changed filename syntax, writing author hash/passwrd, wrote author hash/passwrd, wrote author user name, writing json array, wrote json array, writing instantiation, writing instantiation, writing instantiation, changing constructor syntax, capitalize get function variables, changing getter/setter order, changed autoload prefix, changed capitlization, changed to grouped pairs setter/getter functions, removed redundant possibly autoloader file, changed variables called in constructor, changing capitilization, updated composer locally, updated Uuid namespace, attempting to find errors, changed to json script, changes to author.php, added validate date, rewriting authorhash, c/p author email, c/p author id, c/p para, constructor to find errors, making changes to squash bugs, changed/added brackets due to bracketing error., changed spelling errors, changes trying to squash bugs, changes trying to squash bugs, changes to squash bugs, changes to hashing argon2i, adding code for PDO from example, adding values to PDO string, adding sql insert method, adding delete PDO function, added update PDO function, added arrows where missed, adding PDO object creation script

--- php/Classes/Author.php
+++ php/Classes/Author.php
@@ -315,7 +315,27 @@ class Author implements \JsonSerializable {
 	}
 
 
+	/**
+	 * gets Author by authorId
+	 *
+	 * @param \PDO $pdo PDO connection object
+	 * @param Uuid|string $authorId author id to search for
+	 * @return Author|null author found or null if not found
+	 * @throws \PDOException when mySQL related errors occur
+	 * @throws \TypeError when a variable are not the correct data type
+	 *
+	 **/
+		public static function getAuthorByAuthorId(\PDO $pdo, $authorId) : ?Author {
+			//sanitize the authorId before searching//
+			try {
+					$authorId = self::validateUuid($authorId);
+			} catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
+						throw(new \PDOException($exception->getMessage(), 0, $exception));
+			}
 
+			//create query template//
+			$query = "SELECT authorId, authorAvatarUrl, "
+		}
 
 
 
